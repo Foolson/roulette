1cdc0eefa84abb1882a4f21602a71a18dcbb39db
Johan Olsson <johan.olsson.91@gmail.com>
2015-09-15 09:14:13 +0200

    Added roulette.coo

---
+++ roulette.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	double bank;
+	double account;
+	int    bet;
+	
+	cout << "Welcome to a new game of Roulette!" << endl;
+	account = 1000;
+	cout << "You have " << account << "sek in your account."  << endl;
+	cout << "New bet!" << endl;
+	cout << "Please bet 100, 300 or 500sek." << endl;
+		
+	while(cin >> bet) {
+		if(bet==100 || bet==300 || bet==500) {
+			cout << "Your bet is " << bet << "sek!" << endl;
+			break;
+		}
+		else {
+			cout << "Not a correct input!" << endl;
+		}
+	}
+}
