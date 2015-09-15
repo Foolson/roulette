725d3b0bd2a00b4fe1b8b40a4e3b9991b8c10f4e
Johan Olsson <johan.olsson.91@gmail.com>
2015-09-15 14:31:17 +0200

    The color-part is sort of done

--- roulette.cpp
+++ roulette.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -10,6 +11,11 @@ int main()
 	double account;
 	int    bet;
 	string color_number;
+	string color;
+	int    random_number;
+	bool   even_number;
+	string winning_color;
+	double price;
 	
 	cout << "Welcome to a new game of Roulette!" << endl;
 	account = account + 1000;
@@ -17,7 +23,7 @@ int main()
 	cout << "You have " << account << "sek in your account."  << endl;
 	
 	cout << "New bet!" << endl;
-	cout << "Please bet 100, 300 or 500sek." << endl;
+	cout << "Please bet 100, 300 or 500sek:" << endl;
 	
 	//User gets to input bet	
 	while(cin >> bet) 
@@ -39,26 +45,68 @@ int main()
 	account = account - bet;
 	bank = bank + bet;
 
-	cout << "Do you want to bet on a color or a number?" << endl;
+	cout << "Bet on a color or a number:" << endl;
 	while(cin >> color_number) 
 	{
+		
 		if(color_number=="color"  ||
-		   color_number=="number" ||
-           color_number=="COLOR"  ||
-           color_number=="NUMBER" ||
-           color_number=="Color"  ||
-           color_number=="Number" ||
-           color_number=="c"      ||
-		   color_number=="C"      ||
-		   color_number=="n"      ||
-		   color_number=="N"       ) 
+		   color_number=="number"  ) 
 		{
 			break;			
 		}
 		else 
 		{
-			cout << "Bad!" << endl;
+			cout << "Not a correct input!" << endl;
 		}
+
+	}
+
+	if(color_number=="color")
+	{
+		cout << "Pick red or black:" << endl;
+		while(cin >> color)
+		{
+			if(color=="red"   ||
+               color=="black"  )
+			{
+				break;
+			}
+			else
+			{
+				cout << "Not a correct input!" << endl;
+			}
+		}
+		
+		srand(time(NULL));
+		random_number = rand() % 36 +1;
+		if(random_number % 2 == 0)
+		{
+			winning_color="black";
+		}
+		else
+		{
+			winning_color="red";
+		}
+
+		if(color==winning_color)
+		{
+			price = bet * 2;
+			account = account + price + bet;
+			bank = bank - bet;
+
+			cout << "Winning color: " << winning_color << endl;
+			cout << "You won!" << endl;
+			cout << "A total of " << price << "sek in price money has been moved to your account and your " << bet << "sek bet is back as well." << endl; 
+		}
+		else
+		{
+			cout << "Winning color: " << winning_color << endl;
+			cout << "You lost!" << endl;
+		}
+	}
+	else
+	{
+		
 	}
 	
 }
