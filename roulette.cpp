9876664feceee71da8662a1b4ffefbe917c4c165
Johan Olsson <johan.olsson.91@gmail.com>
2015-09-15 15:27:39 +0200

    Added comments to most of the code

--- roulette.cpp
+++ roulette.cpp
@@ -28,7 +28,7 @@ int main()
 	
 	//User gets to input bet	
 	while(cin >> bet) 
-	{
+	{   //only these three is accepted, letters will make the program crash and i dont know why
 		if(bet==100 && bet<=account ||
            bet==300 && bet<=account ||
 		   bet==500 && bet<=account  ) 
@@ -45,11 +45,12 @@ int main()
 	//Bet moved from account to bank
 	account = account - bet;
 	bank = bank + bet;
-
+	
+	//Player chooses if they want to bet on a color or a number
 	cout << "Bet on a color or a number:" << endl;
 	while(cin >> color_number) 
 	{
-		
+		//Same as in the bet part, ony these two inputs are accepted	
 		if(color_number=="color"  ||
 		   color_number=="number"  ) 
 		{
@@ -61,12 +62,14 @@ int main()
 		}
 
 	}
-
+	
+	//At this place a split begins in the program, color or number
+	//The first one is the color part
 	if(color_number=="color")
-	{
+	{	//Player picks color
 		cout << "Pick red or black:" << endl;
 		while(cin >> color)
-		{
+		{	//Only "red" and "black" are accepted inputs, same method as the other inputs
 			if(color=="red"   ||
                color=="black"  )
 			{
@@ -77,9 +80,10 @@ int main()
 				cout << "Not a correct input!" << endl;
 			}
 		}
-		
+		//A number between 1-36 i randomly generated
 		srand(time(NULL));
 		random_number = rand() % 36 + 1;
+		//To know if the players color is the winning color the code checks if the number is even or odd
 		if(random_number % 2 == 0)
 		{
 			winning_color="black";
@@ -88,28 +92,31 @@ int main()
 		{
 			winning_color="red";
 		}
-
+		
+		//If the players color matches the winning color defined above the player will win
 		if(color==winning_color)
-		{
+		{	//Price money is calculated and the price money + bet money is moved to the players account
 			price = bet * 2;
 			account = account + price + bet;
 			bank = bank - bet;
-
+			
+			//The winning color is declared for the player and that they won, they also get to know how much they have won
 			cout << "Winning color: " << winning_color << endl;
 			cout << "You won!" << endl;
 			cout << "A total of " << price << "sek in price money has been moved to your account and your " << bet << "sek bet is back as well." << endl; 
 		}
 		else
-		{
+		{   //The winning color is declared for the player and that they have lost
 			cout << "Winning color: " << winning_color << endl;
 			cout << "You lost!" << endl;
 		}
 	}
+	//The number part of the split begins here
 	else
-	{
+	{	//The player picks a number between 1-36
 		cout << "Pick a number 1-36:" << endl;
 		while(cin >> number)
-		{
+		{	//Only a number between 1-36 is accepted. Numbers greater than or equal to 1 or numbers lesser or equal to 36
 			if(number >= 1 &&  number <= 36)
 			{
 				break;
@@ -119,21 +126,23 @@ int main()
 				cout << "Not a correct input!" << endl;
 			}
 		}
-
+		//A number between 1-36 is randomly generated
 		srand(time(NULL));
 		random_number = rand() % 36 + 1;
+		//If the random number is the same as the number the player picked they will win
 		if(random_number==number)
-		{
+		{   //Price money is calculated and price + bet money is transfered to the players account
 			price = bet * 10;
 			account = account + price + bet;
 			bank = bank - bet;
-
+			
+			//The winning number is declared and the player and that tey won, they also get to know how much they have won
 			cout << "Winning number: " << random_number << endl;
 			cout << "You Won!" << endl;
 			cout << "A total of " << price << "sek in price money has been moved to your account and your " << bet << "sek bet is back as well." << endl; 
 		}
 		else
-		{
+		{	//The winning number is declared for the player and that they have lost
 			cout << "Winning number: " << random_number << endl;
 			cout << "You lost!" << endl;
 		}
